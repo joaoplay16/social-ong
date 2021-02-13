84bf072518f0591d3b2cd175c4d0efa09c0e6e1c
Joao Pedro <joaoplay16@gmail.com>
2021-02-12 21:00:43 -0300

    bug em inserir frequencia resolvido

--- src/Turma/detailsTurma/index.js
+++ src/Turma/detailsTurma/index.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import api from '../../service/service';//import url base
-
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import api from "../../service/service" //import url base
 
 class DetailsTurma extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       Turma: {
-        nome: ''
+        nome: "",
       },
-      AlunosDaTurma: []
+      AlunosDaTurma: [],
     }
   }
   render() {
-    const { Turma, AlunosDaTurma } = this.state;
+    const { Turma, AlunosDaTurma } = this.state
 
     return (
       <div>
@@ -25,12 +24,14 @@ class DetailsTurma extends Component {
           </div>
 
           <div className="container">
-            <div className="card" style={{ width: '18rem' }}>
+            <div className="card" style={{ width: "18rem" }}>
               <div className="card-header">
                 <b>Alunos da turma: {Turma.nome}</b>
               </div>
               <ul className="list-group list-group-flush">
-                {Object.keys(AlunosDaTurma).length == 0 && <li className="list-group-item" >Lista vazia</li>}
+                {Object.keys(AlunosDaTurma).length == 0 && (
+                  <li className="list-group-item">Lista vazia</li>
+                )}
                 {Object.keys(AlunosDaTurma).map((key) => (
                   <li className="list-group-item d-flex" key={key}>
                     <p>{AlunosDaTurma[key].nomeAluno} </p>
@@ -41,14 +42,26 @@ class DetailsTurma extends Component {
           </div>
         </div>
         <footer>
-          <Link className="btn btn-lg btn-outline-info" to={`/profile/educacao/update-turma/${Turma._id}`}>Editar</Link>
-          <Link className="btn btn-lg btn-outline-danger" to={`/profile/educacao/deletar-turma/${Turma._id}`}>Deletar</Link>
-          <Link className="btn btn-lg btn-outline-secondary" to="/profile/educacao/turmas">Voltar</Link>
+          <Link
+            className="btn btn-lg btn-outline-info"
+            to={`/profile/educacao/update-turma/${Turma._id}`}
+          >
+            Editar
+          </Link>
+          <Link
+            className="btn btn-lg btn-outline-danger"
+            to={`/profile/educacao/deletar-turma/${Turma._id}`}
+          >
+            Deletar
+          </Link>
+          <Link
+            className="btn btn-lg btn-outline-secondary"
+            to="/profile/educacao/turmas"
+          >
+            Voltar
+          </Link>
         </footer>
       </div>
-
-
-
     )
   }
 
@@ -58,34 +71,40 @@ class DetailsTurma extends Component {
   }
 
   async loadTurma() {
-    const { id } = this.props.match.params;
-    console.log("TURMA ", id);
+    const { id } = this.props.match.params
+    console.log("TURMA ", id)
     // pegando o ID da url através do props
-    const response = await api.get(`/Turma/${id}`); //buscar dos dados no banco
+    const response = await api.get(`/Turma/${id}`) //buscar dos dados no banco
     const turma = response.data
 
     this.setState({
-      Turma: turma
+      Turma: turma,
     })
   }
 
   async loadTurmaAluno() {
-    const { id } = this.props.match.params;
-    const response = await api.get(`/TurmaAluno?turma=${id}`);
+    const { id } = this.props.match.params
+    const response = await api.get(`/TurmaAluno?turma=${id}`)
     const { docs: alunosDaTurma } = response.data
 
     //pegando Turma&Aluno e retornando no formato chave valor
-    this.setState({
-      AlunosDaTurma: alunosDaTurma.reduce((accumulator, current) =>
-        Object.assign(/*target*/accumulator,
-          /*source*/{
-            [current.aluno._id]: {
-              nomeAluno: current.aluno.nome,
-              turmaAlunoId: current._id
-            }
-          })
-        , {})
-    })
+    if (alunosDaTurma.length > 0) {
+      this.setState({
+        AlunosDaTurma: alunosDaTurma.reduce(
+          (accumulator, current) =>
+            Object.assign(
+              /*target*/ accumulator,
+              /*source*/ {
+                [current.aluno._id]: {
+                  nomeAluno: current.aluno.nome,
+                  turmaAlunoId: current._id,
+                },
+              }
+            ),
+          {}
+        ),
+      })
+    }
   }
 }
-export default DetailsTurma;
+export default DetailsTurma
