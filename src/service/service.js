0291f86002516ac8030f91a4eb33faf5941cd733
Joao Pedro <joaoplay16@gmail.com>
2021-02-17 19:41:52 -0300

    servidor express adicionado

--- src/service/service.js
+++ src/service/service.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 const PORT = process.env.PORT | 8088
-const HOST =
-  process.env.REACT_APP_NODE_ENV == "production"
-    ? "http://api-projetovamos-com.umbler.net"
-    : `http://localhost:${PORT}`
+const HOST = "http://api-projetovamos-com.umbler.net"
+  // process.env.REACT_APP_NODE_ENV == "production"
+  //   ? "http://api-projetovamos-com.umbler.net"
+  //   : `http://localhost:${PORT}`
 const PATH = "sistema"
 export const API_ADDRESS = `${HOST}/${PATH}`
 export const STATIC_SERVER_ADDRESS = `${HOST}/`
 //url base
 export const api = axios.create({ baseURL: API_ADDRESS })
+
+console.log("ENVIROMENT", process.env.REACT_APP_NODE_ENV);
 
 export default api
