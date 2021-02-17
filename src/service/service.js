d3fa6cc49705e73f6897323898a1076cb5b0cdf7
rosivaldo9 <rosivaldoedeus@gmail.com>
2021-02-17 08:46:00 -0300

    Update service.js

--- src/service/service.js
+++ src/service/service.js
@@ -1,9 +1,6 @@
 import axios from "axios"
 const PORT = process.env.PORT | 8088
-const HOST =
-  process.env.REACT_APP_NODE_ENV == "production"
-    ? "http://api-projetovamos-com.umbler.net"
-    : `http://localhost:${PORT}`
+const HOST = "http://api-projetovamos-com.umbler.net"
 const PATH = "sistema"
 export const API_ADDRESS = `${HOST}/${PATH}`
 export const STATIC_SERVER_ADDRESS = `${HOST}/`
