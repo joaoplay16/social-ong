ab22f6951e894254a17298e76a634fbd97da3b80
Rosivaldo <rosivaldoedeus@gmail.com>
2021-02-20 11:28:57 -0300

    subindo build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94c0130f5b40d2902b6fb7d8cd9cd65a.js"
+  "/precache-manifest.6121a948fcded3cb9f51d0548fed74a2.js"
 );
 
 self.addEventListener('message', (event) => {
