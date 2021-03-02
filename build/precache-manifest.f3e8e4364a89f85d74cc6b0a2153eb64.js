a94073a6d80a914afa9ab090d0a215aa9d26d941
Rosivaldo <rosivaldoedeus@gmail.com>
2021-03-01 21:47:17 -0300

    subindo build

---
+++ build/precache-manifest.f3e8e4364a89f85d74cc6b0a2153eb64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12be628caa1b54940310b8f9e2bc7cbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dab521ef9455eab8138",
+    "url": "/static/css/main.220e3227.chunk.css"
+  },
+  {
+    "revision": "f8f0a763691be536b602",
+    "url": "/static/js/2.6f1c1406.chunk.js"
+  },
+  {
+    "revision": "4845f22d80181185c8b0802564e00bcf",
+    "url": "/static/js/2.6f1c1406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dab521ef9455eab8138",
+    "url": "/static/js/main.ad1f8170.chunk.js"
+  },
+  {
+    "revision": "648dfad5e0063bdc3a98",
+    "url": "/static/js/runtime-main.3ef713f4.js"
+  }
+]);
