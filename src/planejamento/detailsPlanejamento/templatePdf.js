7693c77cebf8c861fcbf4519c8a52afcd2ab6e91
Joao <joaoplay16@gmail.com>
2021-03-02 23:03:15 -0300

    melhorias em relatorio planemento

--- src/planejamento/detailsPlanejamento/templatePdf.js
+++ src/planejamento/detailsPlanejamento/templatePdf.js
@@ -6,110 +6,112 @@ var docDefinition = {}
 
 
 function createLine() {
-    return {
-        canvas: [
-            {
-                type: 'rect',
-                x: 0,
-                y: 10,
-                w: 520,
-                h: 0,
-                r: 8,
-                lineWidth: 2,
-                lineColor: '#CC5',
-            },
-        ]
-    }
+  return {
+    canvas: [
+      {
+        type: 'rect',
+        x: 0,
+        y: 10,
+        w: 520,
+        h: 0,
+        r: 8,
+        lineWidth: 2,
+        lineColor: '#CC5',
+      },
+    ]
+  }
 }
 
 function createSignatureLine() {
-    return {
-        canvas: [
-            {
-                type: 'rect',
-                x: 0,
-                y: 10,
-                w: 320,
-                h: 0,
-                r: 8,
-                lineWidth: 1,
-                lineColor: '#000000',
-            },
-        ],
-        absolutePosition: { y: 799 },
-        alignment: 'center'
-    }
+  return {
+    canvas: [
+      {
+        type: 'rect',
+        x: 0,
+        y: 10,
+        w: 320,
+        h: 0,
+        r: 8,
+        lineWidth: 1,
+        lineColor: '#000000',
+      },
+    ],
+    absolutePosition: { y: 799 },
+    alignment: 'center'
+  }
 }
 
 
 function getFooter() {
-    return {
-
-        columns: [
-            { text: 'Assinatura: ', width: 'auto', margin: [40, 0, 0, 0] },
-            { canvas: createSignatureLine(), width: 'auto' },
-
-        ]
-    }
+  return {
+    
+    columns: [
+      { text: 'Assinatura: ', width: 'auto', margin: [40, 0, 0, 0] },
+      { canvas: createSignatureLine(), width: 'auto' },
+      
+    ]
+  }
 }
 
 
 export function setPdfData(planejamento) {
-    docDefinition = {
-        info: {
-            title: 'awesome Document',
-            author: 'john doe',
-            subject: 'subject of document',
-            keywords: 'keywords for document',
+  docDefinition = {
+    info: {
+      title: 'awesome Document',
+      author: 'john doe',
+      subject: 'subject of document',
+      keywords: 'keywords for document',
+    },
+    // playground requires you to assign document definition to a variable called dd
+    
+    content: [
+      {
+        text: 'Planejamento',
+        style: 'header',
+        alignment: 'center'
+      },
+      {
+        table: {
+          widths: ['*', '*', 100, 100],
+          body: [
+            [{text: 'Atividade', bold: true}, {text: 'Rotina', bold: true}, {text: 'Data do planejamento', bold: true}, {text: 'Data da atividade', bold: true}],
+            [planejamento.atividade, {text: planejamento.rotina}, {text: new Date(planejamento.dataPlanejamento).toLocaleDateString()}, {text: new Date(planejamento.dataAtividade).toLocaleDateString()}],
+          ]
         },
-        content: [
-            {
-                columns: [
-                    { width: 'auto', text: [{ text: 'Aceitacao: ', bold: true }, planejamento.atividade] },
-                    { width: 'auto', text: [{ text: 'Peso: ', bold: true }, 'planejamento.peso' + ' kg',] },
-                    { width: 'auto', text: [{ text: 'Raça: ', bold: true }, 'planejamento.raca',] },
-                    { width: 'auto', text: [{ text: 'Altura: ', bold: true }, 'planejamento.altura',] },
-                ],
-                style: ['label', 'columnStyle'],
-            },        
-
-        ],
-
-        // 	footer: getFooter(),
-
-        styles: {
-            header: {
-                fontSize: 18,
-                bold: true,
-                alignment: 'left',
-                margin: [0, 40, 0, 10]
-            },
-            subheader: {
-                fontSize: 16,
-                bold: true,
-                margin: [0, 10, 0, 0],
-            },
-            label: {
-                fontSize: 15
-            },
-            columnStyle: {
-                columnGap: 20,
-                margin: [0, 10, 0, 0]
-            },
-
+        
+        
+      },
+      {
+        table: {
+          widths: ['*', '*'],
+          body: [
+            [{text: 'Aceitação', bold: true}, {text: 'Observação', bold: true}],
+            [{text: planejamento.aceitacao, italics: true}, {text: planejamento.observacao, italics: true}]
+            
+          ]
         }
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 22,
+        bold: true,
+        alignment: 'justify',
+        marginBottom: 15
+      },
     }
+    
+  }
 }
-
 
 export function downloadPdf() {
-    pdfMake.createPdf(docDefinition).open()
+  pdfMake.createPdf(docDefinition).open()
 }
 export default function createPreview() {
-    const pdfDocGenerator = pdfMake.createPdf(docDefinition);
-    pdfDocGenerator.getBlob((blob) => {
-        let iframe = document.querySelector('iframe')
-        iframe.src = URL.createObjectURL(blob)
-    });
+  const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+  pdfDocGenerator.getBlob((blob) => {
+    let iframe = document.querySelector('iframe')
+    iframe.src = URL.createObjectURL(blob)
+  });
 }
 
