5de00cf8d9f9e9125c7a017c396497f3b257ef3a
Rosivaldo <rosivaldoedeus@gmail.com>
2021-05-21 08:55:23 -0300

    subindo ajuste relatorio

--- src/planejamento/index/indexPlanejamento.js
+++ src/planejamento/index/indexPlanejamento.js
@@ -2,27 +2,67 @@ import React, { Component } from 'react';
 import api from '../../service/service';//import url base
 import { Link } from 'react-router-dom';
 import Tabela from './tabela'
+import jwt_decode from 'jwt-decode';
 import Moment from 'react-moment'
 
 export default class indexplanejamento extends Component {
     
-    state = {
+    constructor() {
+
+        super();
+          this.state = {
         planejamento: [], //dados da tabela
         planejamentoInfo: {},  //informações necessarias para paginação e apresentação dos dados
-        page: 1           //pagina inicial
+        page: 1,           //pagina inicial
+        user : { 
+            nivelUser: 3,
+            user:""
+        }
     };
+  this.user();
+} 
+
+    
+
     componentDidMount() { // metodo executa automatico quando inicia a aplicação
+
         this.loadplanejamento();
+
+    }
+
+    user(){
+
+        const token = localStorage.usertoken
+        const decoded = jwt_decode(token) 
+        if(decoded.nivel == 2){
+		 this.setState({
+            user: {
+                nivelUser: decoded.nivel,
+                user: decoded.nome
+            }
+		 })
+
+         return decoded;
+        }else{
+            return decoded;
+        }
     }
     
     loadplanejamento = async (page = 1) => {
-        const response = await api.get('/planejamento'); //buscar dos dados no banco
-        const { docs, ...planejamentoInfo } = response.data; //armazenando lista do banco em um documento
-        this.setState({planejamento: docs,planejamentoInfo, page }); // setando o estado de Pu.At. com informações da lista do banco 
-    }
+       const user = this.user();
+            if(user.nivel == 2){
+            const response  = await api.get(`/planejamento/${user.nome}`); //buscar dos dados no banco
+            this.setState({planejamento: response.data})
+            }else{
+            const response  = await api.get('/planejamento'); //buscar dos dados no banco
+            const {docs, ...planejamentoInfo } = response.data; //armazenando lista do banco em um documento
+            this.setState({planejamento: docs, ...planejamentoInfo, page})
+            }
+   }
     
     render() {
         const {planejamento} = this.state; // definir variaveis em seu estado atual, carregadas com a lista
+        console.log(this.state.user.user, this.state.user.nivelUser)
         return (
             <>
             <h2 className="text-center">Lista de planejamentos</h2>
