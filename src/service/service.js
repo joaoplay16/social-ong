185e16acb7a4bca51b57b7f5f7ef4484bec3528e
João Pedro <joaoplay16@gmail.com>
2023-12-28 17:11:43 -0300

    Use api host from environment variable

--- src/service/service.js
+++ src/service/service.js
@@ -1,24 +1,18 @@
 import axios from "axios"
-const PORT =  8088
-let HOST =`http://api-projetotesti-com.umbler.net`
-// switch(process.env.REACT_APP_ENVIRONMENT){
-//     case 'production':
-//         HOST = "http://api-projetovamos-com.umbler.net"
-//         break
-//     case 'test':
-//         HOST = "http://api-projetotesti-com.umbler.net"
-//         break 
-//     default:
-//         HOST = `http://localhost:${PORT}`
-// }
+const PORT = 8088
+let HOST = ""
+switch(process.env.REACT_APP_ENVIRONMENT){
+    case 'production':
+        HOST = process.env.REACT_APP_API_HOST
+        break
+    default:
+        HOST = `http://localhost:${PORT}`
+}
 
 const PATH = "sistema"
 export const API_ADDRESS = `${HOST}/${PATH}`
 export const STATIC_SERVER_ADDRESS = `${HOST}/`
 //url base
 export const api = axios.create({ baseURL: API_ADDRESS })
-
-console.log("ENVIROMENT", process.env.REACT_APP_ENVIRONMENT);
-console.log("HOST", HOST);
 
 export default api
